ed9b948d4a9af365768d842e6498b01da340f65a
koseyo <koseyo08@yahoo.co.jp>
2020-08-29 06:36:23 +0900

    fixed form error

--- tweet/views.py
+++ tweet/views.py
@@ -14,7 +14,7 @@ def home_view(request, *args, **kwargs):
 
 def tweet_create_view(request, *args, **kwargs):
     form = TweetForm(request.POST or None)
-    next_url = request.POST.get('next') or None
+    next_url = request.POST.get("next") or None
     if form.is_valid():
         obj = form.save(commit=False)
         obj.save()
@@ -23,7 +23,8 @@ def tweet_create_view(request, *args, **kwargs):
         if next_url != None and is_safe_url(next_url, ALLOWED_HOSTS):
             return redirect(next_url)
         form = TweetForm()
-        if form.errors:
+    if form.errors:
+        if request.is_ajax():
             return JsonResponse(form.errors, status=400)
     return render(request, 'components/form.html', context={"form": form})
 
