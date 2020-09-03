3bfe1f5d537386c494f0bc638d95545a611a308c
koseyo <koseyo08@yahoo.co.jp>
2020-09-03 17:38:50 +0900

    added delete and like tweet

--- tweet/models.py
+++ tweet/models.py
@@ -4,10 +4,17 @@ import random
 
 User = settings.AUTH_USER_MODEL
 
+class TweetLike(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    tweet = models.ForeignKey("Tweet", on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
 class Tweet(models.Model):
     user = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
     content = models.TextField(blank=True, null=True)
     image = models.FileField(upload_to='images/', blank=True, null=True)
+    like = models.ManyToManyField(User, related_name='tweet_user', blank=True, through=TweetLike)
+    timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.content
